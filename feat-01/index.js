a4a223b555d7f9dca54f2b98c2d9a11d744ccd65
ChelesteWang <1581446178@qq.com>
2022-02-15 22:43:29 +0800

    2 files modified

--- feat-01/index.js
+++ feat-01/index.js
@@ -1,10 +1,22 @@
-export function main() {
+export default function main() {
   console.log('准备烹饪');
-  function cook(resolve, reject) {
-    console.log('开始洗菜');
-    resolve();
+  function cook(stage, resolve, reject) {
+    setTimeout(() => {
+      console.log(stage);
+      resolve();
+    }, 2000);
   }
-  cook(()=>{
-    console.log('开始切菜')
-  },);
+  cook('开始切菜', () => {
+    cook('开始炒菜',()=>{
+      cook('起锅烧油',()=>{
+        cook('放入调料',()=>{
+          cook('翻炒均匀',()=>{
+            cook('装盘上桌',()=>{
+             console.log('结束烹饪')
+            })
+          })
+        })
+      })
+    })
+  });
 }
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import stylesheets
 import './style.css';
 
+import feat1 from './feat-01'
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+feat1()
