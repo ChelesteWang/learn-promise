8b6e59eb2ea999c48d004391c9553632154775e9
ChelesteWang <1581446178@qq.com>
2022-02-16 21:14:48 +0800

    1 file modified

--- feat-04/index.js
+++ feat-04/index.js
@@ -1,2 +1,8 @@
-const success = Promise.resolve('成功')
-const fail = Promise.reject('失败')
+const success = Promise.resolve('成功');
+const fail = Promise.reject('失败');
+
+// 并发执行全部返回都为成功才成功
+const data = Promise.all(success, fail);
+
+// 并发执行第一个返回结果决定整体结果
+const result = Promise.race(success,fail)
