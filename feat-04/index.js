200159005c86865a750cd8387618ef5221504d21
ChelesteWang <1581446178@qq.com>
2022-02-16 21:42:06 +0800

    2 files modified

--- feat-04/index.js
+++ feat-04/index.js
@@ -1,8 +1,34 @@
-const success = Promise.resolve('成功');
-const fail = Promise.reject('失败');
+export default function main() {
+  const success = Promise.resolve('成功');
+  const fail = Promise.reject('失败');
 
-// 并发执行全部返回都为成功才成功
-const data = Promise.all(success, fail);
+  success.then((res) => {
+    console.log(res);
+  });
 
-// 并发执行第一个返回结果决定整体结果
-const result = Promise.race(success,fail)
+  fail.catch((res) => {
+    console.log(res);
+  });
+
+  // 并发执行全部返回都为成功才成功
+  const data = Promise.all([success, fail]);
+  data
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  // 并发执行第一个返回结果决定整体结果
+  const result = Promise.race([success, fail]);
+  result
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log('测试 Promise all race');
+}
--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 import './style.css';
 
 import feat1 from './feat-01'
-import feat3 from './feat-03'
+import feat4 from './feat-04'
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-feat3()
+feat4()
