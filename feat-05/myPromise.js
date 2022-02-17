a2e1a438d76de5b77f70c14442ed3e79c9448865
ChelesteWang <1581446178@qq.com>
2022-02-17 20:14:17 +0800

    2 files modified

--- feat-05/myPromise.js
+++ feat-05/myPromise.js
@@ -1,4 +1,39 @@
-export default class MyPromise{
+const PENDING = 'pending';
+const REJECTED = 'rejected';
+const FULFILLED = 'fulfilled';
 
-  
+export default class MyPromise {
+  constructor(executor) {
+    executor(this.resolve, this.reject);
+  }
+
+  status = PENDING;
+
+  value = undefined;
+
+  reason = undefined;
+
+  resolve = (value) => {
+    if (this.status !== PENDING) {
+      return;
+    }
+    this.status = REJECTED;
+    this.value = value;
+  };
+
+  reject = (reason) => {
+    if (this.status !== PENDING) {
+      return;
+    }
+    this.status = FULFILLED;
+    this.reason = reason;
+  };
+
+  then(successCallback, failCallback) {
+    if (this.status === REJECTED) {
+      successCallback(this.value);
+    } else if (this.status === FULFILLED) {
+      failCallback(this.reason);
+    }
+  }
 }
