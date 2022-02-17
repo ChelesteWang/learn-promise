944da091df6220363161cc3448cee299205edad9
ChelesteWang <1581446178@qq.com>
2022-02-17 20:24:28 +0800

    1 file modified

--- feat-05/index.js
+++ feat-05/index.js
@@ -1,10 +1,10 @@
 import myPromise from './myPromise'
 export default function main() {
   new Promise((resolve, reject) => {
-    resolve('成功');
+    resolve('promise 成功');
   }).then((res) => console.log(res));
 
   new myPromise((resolve, reject) => {
-    resolve('成功');
+    resolve('my promise 成功');
   }).then((res) => console.log(res));
 }
